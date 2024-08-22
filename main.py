d672f2af352b0ca375fea5452e9d1e352a43ddc1
DJZAMPA <aryanrehal4@gmail.com>
2024-08-22 15:15:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
-import sys
-import os
+from flask import Flask
+from threading import Thread
+from highrise.__main__ import *  # Ensure the necessary imports from highrise
 import time
-import traceback
 from importlib import import_module
-from highrise.__main__ import *
+from asyncio import run as arun
 
-# BOT SETTINGS
-bot_file_name = "xenoichi"
-bot_class_name = "xenoichi"
-room_id = "65e93ab505782ba2fe3745c7"
-bot_token ="559e0fc1599e679b49704bd55939f00dc57057a41a976826d52afebcee9b22ad"
+class WebServer:
+    def __init__(self):
+        self.app = Flask(__name__)
 
-my_bot = BotDefinition(getattr(import_module(bot_file_name), bot_class_name)(), room_id, bot_token)
+        @self.app.route('/')
+        def index() -> str:
+            return "Funcionando"
 
-while True:
-    try:
-        definitions = [my_bot]
-        arun(main(definitions))
-    except Exception as e:
-        print(f"An exception occurred: {e}")
-        traceback.print_exc()
-    time.sleep(5)
+    def run(self) -> None:
+        self.app.run(host='0.0.0.0', port=8080)
 
+    def keep_alive(self):
+        t = Thread(target=self.run)
+        t.start()
+
+class RunBot:
+    room_id = "65e93ab505782ba2fe3745c7"
+    bot_token = "559e0fc1599e679b49704bd55939f00dc57057a41a976826d52afebcee9b22ad"
+    bot_file = "xenoichi"
+    bot_class = "xenoichi"
+
+    def __init__(self) -> None:
+        self.definitions = [
+            BotDefinition(
+                getattr(import_module(self.bot_file), self.bot_class)(),
+                self.room_id, self.bot_token)
+        ]  # More BotDefinition classes can be added to the definitions list
+
+    def run_loop(self) -> None:
+        while True:
+            try:
+                arun(main(self.definitions))
+            except Exception as e:
+                print("Error: ", e)
+                time.sleep(5)
+
+if __name__ == "__main__":
+    # Start the Flask web server
+    WebServer().keep_alive()
+
+    # Start the bot
+    RunBot().run_loop()
