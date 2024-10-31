d7235133f9e53833004c81f8e5bc05c0ae7a6eaa
DJZAMPA <aryanrehal4@gmail.com>
2024-10-31 11:14:15 +0530

    Update xenoichi.py

--- xenoichi.py
+++ xenoichi.py
@@ -46,7 +46,7 @@ class xenoichi(BaseBot):
     async def on_chat(self, user: User, message: str) -> None:
 
 
-        if user.username in {"DJ._.ZAMPA", "spy__x", "SUNLIGHT._.1", "Aanandi_raj_"}:
+        if user.username in {"DJ._.ZAMPA", "spy__x", "SUNLIGHT._.1", "Aanandi_raj_", "YOUR_friend.RRJ"}:
     # Do something if the username matches
     
 
